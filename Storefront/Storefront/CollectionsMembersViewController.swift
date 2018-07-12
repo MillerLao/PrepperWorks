aba6ca613a27696e15142e81ab0affe1873788af
Wyatt <waylon@hkp3.com>
2018-07-12 19:10:20 -0400

    Add MemberViewController

---
+++ Storefront/Storefront/CollectionsMembersViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionsMembersViewController.swift
+//  Storefront
+//
+//  Created by HKP3 Waylon on 7/12/18.
+//  Copyright © 2018 Shopify Inc. All rights reserved.
+//
+
+import UIKit
+import Buy
+
+
+class CollectionsMembersViewController: UIViewController {
+    
+    
+    
+    
+}
