a00f3bd04f8292ce7dbefce85d74306901404ef2
HKP3 Waylon <hkp3waylon@hkp3s-macbook-air-3.local>
2018-07-12 11:47:47 -0400

    Add SignUpViewController and link to the respective storyboard scene

---
+++ Storefront/Storefront/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  Storefront
+//
+//  Created by HKP3 Waylon on 7/12/18.
+//  Copyright © 2018 Shopify Inc. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Stuff to do after the view loads
+    }
+    
+    
+    
+}
